800cf2b29f093f6e187ff030fcf1da75f89da1ed
AndrewBo29 <andrewbo29@yandex.ru>
2013-02-19 02:44:04 +0400

    first commit

---
+++ recArabicWord.py
@@ -0,0 +1,62 @@
+# -*- coding: UTF-8 -*-
+
+import numpy as np
+import cv2
+import arabic_reshaper
+import sys
+
+THRESHOLD = 5
+RESULT_PATH = 'result.doc'
+
+def readImage(path):
+    bim = cv2.imread(path, cv2.CV_LOAD_IMAGE_GRAYSCALE)
+    im = abs(bim / 255 - 1)
+    return im
+
+def simpleCompareImages(image1, image2):
+    val = np.mean(abs(image1 - image2))
+    if val <= THRESHOLD:
+        return True
+    return False
+
+def get_dictionary():
+    arabicWords = [arabic_reshaper.reshape(u'وزارة'), arabic_reshaper.reshape(u'الشؤون'),
+                   arabic_reshaper.reshape(u'الخارجيّة')]
+    # , arabic_reshaper.reshape(u'الشؤون'),
+    # arabic_reshaper.reshape(u'الشؤون'), arabic_reshaper.reshape(u'الشؤون'),
+    # arabic_reshaper.reshape(u'الشؤون'), arabic_reshaper.reshape(u'الشؤون')]
+
+    return arabicWords
+
+def get_image_dictionary():
+    arabicImagePaths = ["image_represent\word1.bmp", "image_represent\word2.bmp",
+                        "image_represent\word3.bmp"]
+
+    return arabicImagePaths
+
+def get_split_string(path):
+    splitWords = ["image_represent\word1.bmp", "image_represent\word2.bmp",
+                        "image_represent\word3.bmp"]
+
+    return splitWords
+
+def process(words, dictImages, dict):
+    f = open(RESULT_PATH, 'w')
+    resultStr = ''
+    for i in range(0, len(words)):
+        word = readImage(words[i])
+        for j in range(0, len(dictImages)):
+            wordDict = readImage(dictImages[j])
+            if simpleCompareImages(word, wordDict):
+                resultStr += dict[j] + ' '
+    f.write(resultStr)
+    f.close()
+    print('Result: ' + resultStr)
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+arabicWords = get_dictionary()
+arabicImagePaths = get_image_dictionary()
+words = get_split_string('')
+process(words, arabicImagePaths, arabicWords)
